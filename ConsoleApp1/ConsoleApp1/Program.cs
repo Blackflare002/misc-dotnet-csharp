a864d098f6031c97125670a5d808a34166272f42
Shane Phillips <shane@loongphillips.com>
2022-10-11 15:31:31 -0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Spooky ghost ASCII art
+Console.WriteLine(" .-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| o |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
+Console.WriteLine("Spookums");
---
+++ ascii-ghost.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Spooky ghost ASCII art
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| o |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("Spookums");
+        }
+    }
+}
